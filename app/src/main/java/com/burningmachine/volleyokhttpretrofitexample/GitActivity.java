c22a3064e85e59a4fcea49234cd20b338a4661c7
vipinsharma121 <vipin.vashistha@gmail.com>
2018-12-05 16:07:41 +0530

    add variable

--- app/src/main/java/com/burningmachine/volleyokhttpretrofitexample/GitActivity.java
+++ app/src/main/java/com/burningmachine/volleyokhttpretrofitexample/GitActivity.java
@@ -8,6 +8,6 @@ public class GitActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_git);
+        setContentView(R.layout.activity_retrofit);int i=6;
     }
 }
