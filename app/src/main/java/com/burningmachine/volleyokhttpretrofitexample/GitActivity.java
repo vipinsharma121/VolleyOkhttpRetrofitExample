088b2e41f1b41a490754beac2b55219ed45457cd
Praveen <psharma.cs@gmail.com>
2018-12-05 16:26:13 +0530

    commited....

--- app/src/main/java/com/burningmachine/volleyokhttpretrofitexample/GitActivity.java
+++ app/src/main/java/com/burningmachine/volleyokhttpretrofitexample/GitActivity.java
@@ -2,15 +2,19 @@ package com.burningmachine.volleyokhttpretrofitexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class GitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_retrofit);
-        int i=6;int k=20;
+        setContentView(R.layout.activity_retrofit);int i=6;
 
 
+    }
+    public void show()
+    {
+        Log.e("msg----------","Message");
     }
 }
