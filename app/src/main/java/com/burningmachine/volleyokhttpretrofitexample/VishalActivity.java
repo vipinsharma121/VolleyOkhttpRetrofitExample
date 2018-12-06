9db5325a1d313fc259450b603ce41ae244b1e7f6
Praveen <psharma.cs@gmail.com>
2018-12-06 12:38:30 +0530

    vishal new

---
+++ app/src/main/java/com/burningmachine/volleyokhttpretrofitexample/VishalActivity.java
@@ -0,0 +1,13 @@
+package com.burningmachine.volleyokhttpretrofitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VishalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vishal);
+    }
+}
