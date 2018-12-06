9b4a772927734bccd73c6cde5ebdd6726c34dd9f
vipinsharma121 <vipin.vashistha@gmail.com>
2018-12-06 12:32:31 +0530

    bbbbbbbjjjj

--- app/src/main/java/com/burningmachine/volleyokhttpretrofitexample/EmptyActivity.java
+++ app/src/main/java/com/burningmachine/volleyokhttpretrofitexample/EmptyActivity.java
@@ -10,5 +10,11 @@ public class EmptyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty2);
         int z=1212;
+         int jj=20000;
+         int j=20000;
+         int hj=20000;
+         int bhn=20000;
+         int bhjin=20000;
+
     }
 }
